21e2773390af425f08255b8ee9c518360ac2bd67
iqbalaulia <miqbalar12@gmail.com>
2022-10-08 11:11:09 +0700

    [update] config

---
+++ src/views/crafted/pages/home/PPHome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   tesitngg
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  components: {
+    // Navbar: () => import(`../../layout/Navbar/PPNavbar.vue`),
+    // // FooterLayout: () => import(`./FooterBar/PPFooter.vue`),
+  },
+};
+</script>
