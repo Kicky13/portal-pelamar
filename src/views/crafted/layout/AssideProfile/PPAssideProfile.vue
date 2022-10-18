538cb904c2820edd07550b029f2a76be7b58897e
iqbalaulia <miqbalar12@gmail.com>
2022-10-19 04:33:32 +0700

    [update] accordion menu

--- src/views/crafted/layout/AssideProfile/PPAssideProfile.vue
+++ src/views/crafted/layout/AssideProfile/PPAssideProfile.vue
@@ -22,12 +22,73 @@
     <label for="">My Job</label>
     <ul>
       <li>
-        <router-link to="" class="d-flex align-items-center">
-          <img
-            :src="require('@/assets/images/icon/ic_detail.svg')"
-            class="img-fluid"
-            alt="" /><span>Detail CV</span>
-        </router-link>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="kt_accordion_1_header_2">
+            <a href="javascript:void(0)"
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#kt_accordion_1_body_2"
+              aria-expanded="false"
+              aria-controls="kt_accordion_1_body_2">
+              <img
+                :src="require('@/assets/images/icon/ic_detail.svg')"
+                class="img-fluid"
+                alt="" /><span>Detail CV</span>
+            </a>
+          </h2>
+          <div
+            id="kt_accordion_1_body_2"
+            class="accordion-collapse collapse"
+            aria-labelledby="kt_accordion_1_header_2"
+            data-bs-parent="#kt_accordion_1">
+            <div class="accordion-body">
+
+              <ul style="margin-left: 36px; margin-top:10px;">
+                <li>
+                  <router-link to="#">
+                    <span>Data Personal</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Personifikasi</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Riwayat Pekerjaan</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Keanggotaan Organisasi</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Pendidikan & Pelatihan</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Publikasi</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Narasumber</span>
+                  </router-link>
+                </li>
+                <li>
+                  <router-link to="#">
+                    <span>Referensi</span>
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </li>
       <li>
         <router-link to="/profile/resume-cv" class="d-flex align-items-center">
