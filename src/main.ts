003477117bb7e79eed5a8a2048a79df0c9bcc05a
iqbalaulia <miqbalar12@gmail.com>
2022-11-11 14:31:27 +0700

    [update] fixing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import BootstrapVue3 from 'bootstrap-vue-3'
 
 /*
 TIP: To get started with clean router change path to @/router/clean.ts.
@@ -18,11 +19,14 @@ import { initVeeValidate } from "@/core/plugins/vee-validate";
 import "@/core/plugins/prismjs";
 import "bootstrap";
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
 app.use(ElementPlus);
+app.use(BootstrapVue3)
 
 ApiService.init(app);
 initApexCharts(app);
